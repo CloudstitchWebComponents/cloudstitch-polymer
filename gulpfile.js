475a6e1d7aa2f07ac5ab85cb28530732d08fe3d8
Ted Benson <ted@cloudstitch.io>
2015-12-03 11:39:46 -0800

    Fixing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -33,8 +33,8 @@ gulp.task('copy', function () {
     'bower_components/**/*'
   ]).pipe(gulp.dest('dist'));
 
-  var vulcanized = gulp.src(['cloudstitch-d3.html'])
-    .pipe($.rename('cloudstitch-d3.vulcanized.html'))
+  var vulcanized = gulp.src(['cloudstitch-polymer.html'])
+    .pipe($.rename('cloudstitch-polymer.vulcanized.html'))
     .pipe(gulp.dest('dist/elements'));
 });
 
@@ -43,7 +43,7 @@ gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 // Vulcanize imports
 gulp.task('vulcanize', function () {
   var DEST_DIR = '.';
-  return gulp.src('dist/elements/cloudstitch-d3.vulcanized.html')
+  return gulp.src('dist/elements/cloudstitch-polymer.vulcanized.html')
     .pipe($.vulcanize({
       stripComments: true,
       inlineCss: true,
@@ -74,7 +74,7 @@ gulp.task('deploy:remote', function() {
       Bucket: 'components.cloudstitch.com', //  Required
       ACL:    'public-read',
       keyTransform: function(relative_filename) {
-        var fname = 'elements/' + location + '/cloudstitch-d3/' + relative_filename;
+        var fname = 'elements/' + location + '/cloudstitch-polymer/' + relative_filename;
         console.log(fname);
         return fname;
       }
